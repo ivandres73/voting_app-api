7318dfd5aa8df3412390dcb621c14e9287fb00a3
Ivan Carcamo <ivandres73@unitec.edu>
2018-03-19 09:23:27 -0700

    added 4 defaults products

--- voting_app-api/db/seeds.rb
+++ voting_app-api/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create(
+	[
+		{
+			title: "Yellow Pail",
+			description: "On-demand sand castle construction expertise.",
+			votes: 0,
+			submitterAvatarURL: "avatars/daniel.jpg",
+      		productImageUrl: "products/image-aqua.png"
+		},
+		{
+			title: "Supermajority: The Fantasy Congress League",
+        	description: "Earn points when your favorite politicians pass legislation.",
+      		votes: 0,
+      		submitterAvatarURL: "avatars/kristy.png",
+      		productImageUrl: "products/image-rose.png"
+		},
+		{
+			title: "Tinfoild: Tailored tinfoil hats",
+        	description: "We already have your measurements and shipping address.",
+      		votes: 0,
+      		submitterAvatarURL: "avatars/veronika.jpg",
+      		productImageUrl: "products/image-steel.png"
+      	},
+      	{
+      		title: "Haught or Naught",
+        	description: "High-minded or absent-minded? You decide.",
+      		votes: 0,
+      		submitterAvatarURL: "images/avatars/molly.png",
+      		productImageUrl: "images/products/image-yellow.png"
+      	}
+
+	]
+)
