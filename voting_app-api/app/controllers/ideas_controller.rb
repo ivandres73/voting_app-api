e5aa1e66231ca16dce7a42bf0ba19e9ecafc0999
Ivan Carcamo <ivandres73@unitec.edu>
2018-03-19 10:26:29 -0700

    added gem rack-corse; added other methods in controller (not tested)

--- voting_app-api/app/controllers/ideas_controller.rb
+++ voting_app-api/app/controllers/ideas_controller.rb
@@ -4,4 +4,21 @@ class IdeasController < ApplicationController
 		@products = Product.all
 		render json: @products
 	end
+
+	def create
+		@product = Product.create(prod_params)
+		render json: @products
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		@product.update_attributes(prod_params)
+
+		render json: @products
+	end
+
+	private
+		def prod_params
+			params.require(:product).permit(:title, :description, :votes)
+		end
 end
