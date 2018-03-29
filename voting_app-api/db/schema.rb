6674402d2ba1a739a0324c3df5188ca2866cc44a
Ivan Carcamo <ivandres73@unitec.edu>
2018-03-29 00:40:45 -0700

    changed sqlite to postgre

--- voting_app-api/db/schema.rb
+++ voting_app-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180316010135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
