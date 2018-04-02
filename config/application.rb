76d9461787ca0a21545f7bc07c41f5115c5a5a5e
renanz <renan_zelaya@unitec.edu>
2018-04-01 20:27:44 -0600

    heroku:

--- config/application.rb
+++ config/application.rb
@@ -37,12 +37,8 @@ module VotingAppApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://votingapp-frontend.herokuapp.com/'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
-      end
-      allow do
-          origins 'http://localhost:3000'
-          resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
     
